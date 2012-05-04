efc956a76c7f0265cb30f9a60e183df550daab8f
Kyle Turner <kyle@nightowl-llc.com>
2012-05-03 19:25:27 -0500

    First commit

---
+++ iOSWorkshop/iOSWorkshop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSWorkshop
+//
+//  Created by Kyle Turner on 5/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSAppDelegate class]));
+    }
+}
